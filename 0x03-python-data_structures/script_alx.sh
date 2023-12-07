520f122c30e1504316615b86439566ae1e86cc13
Abdul Rahman <yhungmhansuccesssor@gmail.com>
2023-12-07 02:35:01 -0500

    2-replace_in_list.py task done

--- 0x03-python-data_structures/script_alx.sh
+++ 0x03-python-data_structures/script_alx.sh
@@ -4,6 +4,15 @@ vi $pyfile
 chmod u+x $pyfile
 pycodestyle --first $pyfile
 ./$pyfile
+
+read -p "$pyfile has a main? [yes/no]: " main
+if [ "$main" = "yes" ]; then
+	read -p "Enter task number: " task
+	task="$task-main.py"
+	vi $task
+	chmod u+x $task
+	./$task
+fi
 echo -e "\n\n======== Push or Correction =========\n"
 read -p "Continue? [yes/no]: " cont
 if [ "$cont" = "yes" ]; then
